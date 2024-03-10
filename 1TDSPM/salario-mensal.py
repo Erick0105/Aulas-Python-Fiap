3e0cc6e98841ae512a8f38ce948347d030474ed0
Erick Alves <161581322+Erick0105@users.noreply.github.com>
2024-03-10 17:40:31 -0300

    informacoes pedidas

---
+++ 1TDSPM/salario-mensal.py
@@ -0,0 +1,5 @@
+print("Vamos ajudar a calcular o seu salário mensal!")
+
+horas_trab = int(input("Qual a sua quantidade de horas trabalhadas no mês?"))
+valor_hora = float(input("Qual seria o valor da sua hora trabalhada?"))
+percen_desc = int(input("Quanto foi o seu percentual de desconto no mês?"))
