c88c8fa8e96c99ca31ee943b560bc41b0f47bd40
Erick Alves <161581322+Erick0105@users.noreply.github.com>
2024-03-10 17:54:23 -0300

    orginazacao

--- 1TDSPM/salario-mensal.py
+++ 1TDSPM/salario-mensal.py
@@ -1,13 +1,13 @@
 print("Vamos ajudar a calcular o seu salário mensal!")
 
-horas_trab = int(input("Qual a sua quantidade de horas trabalhadas no mês?"))
-valor_hora = float(input("Qual seria o valor da sua hora trabalhada?"))
-percen_desc = int(input("Quanto foi o seu percentual de desconto no mês?"))
+horas_trab = int(input("Qual a sua quantidade de horas trabalhadas no mês? "))
+valor_hora = float(input("\nQual seria o valor da sua hora trabalhada? "))
+percen_desc = int(input("\nQuanto foi o seu percentual de desconto no mês? "))
 
 salario_bruto = horas_trab * valor_hora
 total_desc = (percen_desc * salario_bruto)/100
 salario_liquido = salario_bruto - total_desc
 
-print(f'O senhor possui salário bruto de {salario_bruto}.')
-print(f'E como o senhor possui um total de desconto igual a {total_desc}')
-print(f'Então seu Slário final será igual a {salario_liquido}')
+print(f'\nO senhor possui salário bruto de {salario_bruto}.')
+print(f'\nE como o senhor possui um total de desconto igual a {total_desc}')
+print(f'\n Então seu Slário final será igual a {salario_liquido}')
