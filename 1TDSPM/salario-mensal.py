306d278e544501fa275c2f512b34e9c9ea82cfce
Erick Alves <161581322+Erick0105@users.noreply.github.com>
2024-03-10 17:43:49 -0300

    finalização

--- 1TDSPM/salario-mensal.py
+++ 1TDSPM/salario-mensal.py
@@ -7,3 +7,7 @@ percen_desc = int(input("Quanto foi o seu percentual de desconto no mês?"))
 salario_bruto = horas_trab * valor_hora
 total_desc = (percen_desc * salario_bruto)/100
 salario_liquido = salario_bruto - total_desc
+
+print(f'O senhor possui salário bruto de {salario_bruto}.')
+print(f'E como o senhor possui um total de desconto igual a {total_desc}')
+print(f'Então seu Slário final será igual a {salario_liquido}')
