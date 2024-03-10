11f322c92fa6ae132bf43ab784bfa77574ab74ac
Erick Alves <161581322+Erick0105@users.noreply.github.com>
2024-03-10 19:30:16 -0300

    retorno

--- 1TDSPM/IMC.py
+++ 1TDSPM/IMC.py
@@ -4,3 +4,8 @@ peso = float(input("Qual o seu peso atual? "))
 altura = float(input("\nQual a sua altura? "))
 
 IMC = peso / altura**2
+
+if IMC > 25:
+    print("Você está em estado de Sobrepeso")
+else:
+    print("você está bem com seu peso atual")
