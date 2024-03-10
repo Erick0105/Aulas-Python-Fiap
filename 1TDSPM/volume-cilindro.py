5fdb24311c61b2f6b41365507d143cdf37d8933a
Erick Alves <161581322+Erick0105@users.noreply.github.com>
2024-03-10 19:02:11 -0300

    entrega var's

---
+++ 1TDSPM/volume-cilindro.py
@@ -0,0 +1,4 @@
+print("\nVamos calcular o volume de um cilindro!!!\n\n")
+
+raio = float(input("Qual seria o valor do raio da base do cilindro "))
+altura = float(input("\nE Qual seria a altura do cilindro? "))
