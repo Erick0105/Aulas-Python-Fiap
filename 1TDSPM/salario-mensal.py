666d479b2b0ccc864539f92c34d8cad63af45e82
Erick Alves <161581322+Erick0105@users.noreply.github.com>
2024-03-10 17:42:46 -0300

    processamento infor

--- 1TDSPM/salario-mensal.py
+++ 1TDSPM/salario-mensal.py
@@ -3,3 +3,7 @@ print("Vamos ajudar a calcular o seu salário mensal!")
 horas_trab = int(input("Qual a sua quantidade de horas trabalhadas no mês?"))
 valor_hora = float(input("Qual seria o valor da sua hora trabalhada?"))
 percen_desc = int(input("Quanto foi o seu percentual de desconto no mês?"))
+
+salario_bruto = horas_trab * valor_hora
+total_desc = (percen_desc * salario_bruto)/100
+salario_liquido = salario_bruto - total_desc
