c09579a0bedb825173bdf0caaf31d003f37d1e41
Erick Alves <161581322+Erick0105@users.noreply.github.com>
2024-03-10 18:01:44 -0300

    adquirir variaveis

---
+++ 1TDSPM/equacao-2grau.py
@@ -0,0 +1,5 @@
+print("Vamos descobrir os valores de ambos os X desta equação para você")
+
+a_valor = int(input("Qual o valor de 'a'?(O valor que acompanha o X²) "))
+b_valor = int(input("\nQual o valor de 'b'?(O valor que acompanha o X) "))
+c_valor = int(input("\nE qual o valor de 'c'?(O valor sozinho.) "))
