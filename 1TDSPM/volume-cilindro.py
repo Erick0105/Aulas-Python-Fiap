95d51c1400d391ea072544e45d1c602743458270
Erick Alves <161581322+Erick0105@users.noreply.github.com>
2024-03-10 19:04:20 -0300

    processamento

--- 1TDSPM/volume-cilindro.py
+++ 1TDSPM/volume-cilindro.py
@@ -2,3 +2,6 @@ print("\nVamos calcular o volume de um cilindro!!!\n\n")
 
 raio = float(input("Qual seria o valor do raio da base do cilindro "))
 altura = float(input("\nE Qual seria a altura do cilindro? "))
+
+area_base = raio**2 * 3.14
+volume = area_base * altura
