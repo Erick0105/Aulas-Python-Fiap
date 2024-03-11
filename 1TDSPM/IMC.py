ea722c0e598c087c42e9920dbeff73f0199617ae
Erick0105 <erickspot01@gmail.com>
2024-03-11 08:51:54 -0300

    att retorno

--- 1TDSPM/IMC.py
+++ 1TDSPM/IMC.py
@@ -1,11 +1,13 @@
-print("Vamos calcular seu IMC\n\n")
+print("Programa para calcular seu IMC\n\n")
 
 peso = int(input("Qual o seu peso atual? "))
-altura = int(input("\nQual a sua altura em centimetros? "))
+altura = float(input("\nQual a sua altura em centimetros? "))
 
 IMC = peso / altura**2
 
 if IMC > 25:
-    print("Você está em estado de Sobrepeso")
+    print("\nVocê está em estado de Sobrepeso")
+elif IMC <= 18.4:
+    print("\nVocê está em estado de magreza tome cuidado com sua saúde")
 else:
-    print("você está bem com seu peso atual")
+    print("\nvocê está bem com seu peso atual")
