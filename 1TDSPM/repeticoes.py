788250eb330b289b2a3c6110dd426b035bfaf775
Erick0105 <erickspot01@gmail.com>
2024-03-18 08:53:30 -0300

    tabuada do 7

--- 1TDSPM/repeticoes.py
+++ 1TDSPM/repeticoes.py
@@ -1,10 +1,21 @@
 print("Inicio do programa")
+
 n = 1
 while n < 11:
     print(f'linha {n}')
-    n = n+1
-print("Fim do programa")
+    n += 1
+print("\n")
 
-for x in range(10):
-    x = x + 1
-    print(f'Linha {x}')
+for x in range(11):
+    print(f'Linha {x}') 
+print("\n")
+
+numero = 0
+while numero <= 20:
+    print(numero)
+    numero += 2
+print("\n")
+
+for i in range(11):
+    n = i * 7
+    print(f'7 X {i} = {n}')
