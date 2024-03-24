2a0089a6448b648df551f7c3670d2ed1aea94d1d
Erick0105 <erickspot01@gmail.com>
2024-03-24 20:35:44 -0300

    organizacao

--- myself/figuras-geometricas-medidas.py
+++
@@ -1,9 +0,0 @@
-print("Vamos descobrir o valor de área de algumas formas geometricas")
-
-decisao = {'círculo':1,'triangulo':2,'quadrado':3,'retangulo':4,'trapezio':5,'losango': 6,'pentagono':7,'hexagono':8,\
-    'octogono': 9,'eneagono': 10}
-
-escolha = int(input(f'{decisao}\n Qual das figuras geometricas acima você quer descobrir a área?'))
-
-if escolha == 1:
-    
