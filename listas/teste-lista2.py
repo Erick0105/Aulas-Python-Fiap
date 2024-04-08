f245a15b7e774a56ead9551de299b6c3ab096ab5
Erick0105 <erickspot01@gmail.com>
2024-04-08 08:53:22 -0300

    função try

--- listas/teste-lista2.py
+++ listas/teste-lista2.py
@@ -1,5 +1,16 @@
 lista = ["Isabela", "Guilherme", "Juliana", "Pablo", "Mikael", "Enzo", "Vicenzo", "Guilherme"]
 
-pos = lista.index("Pablo")
-
+pos = lista.index("Pablo") #localiza o item da lista
 print(f'O pablo está na posição {pos}')
+
+pos = lista.index("Guilherme")
+print(f'O Guilherme está na posição {pos}')
+
+pos = lista.index("Guilherme", 2)
+print(f'O segundo Guilherme está na posição {pos}')
+
+try:
+    pos = lista.index("João")
+    print(f'O joão esta na posição: {pos}')
+except:
+    print("Não há João na Lista")
