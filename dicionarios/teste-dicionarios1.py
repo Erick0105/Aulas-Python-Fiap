fdd4c3b947e387b23e7a598b6a5071e1f6170767
Erick0105 <erickspot01@gmail.com>
2024-04-15 08:29:42 -0300

    print de chaves

---
+++ dicionarios/teste-dicionarios1.py
@@ -0,0 +1,22 @@
+d1 = {
+    "alunos": 40,
+    "professor": "Antonio",
+    "computadores": 43,
+    "cadeiras": 43,
+    "monitores": 43,
+    "perifericos": {"computadores": 43, "monitores": 43}
+}
+d1["sala_aula"] = 201
+# print(d1)
+if "sala_aula" in d1:
+    print("Existe a chave 'sala_aula' no dicionario")
+else:
+    print("Não existe a chave 'sala_aula' no dicionario")
+
+#tamanho = len(d1.keys)
+#for i in range(tamanho):
+#    chave = d1.keys[i]
+#    print(chave)
+
+for chave in d1.keys():
+    print(chave)
