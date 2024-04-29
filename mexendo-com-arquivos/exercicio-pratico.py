875bc34e9d4f8219da702bf564960f757da8bb85
Erick0105 <erickspot01@gmail.com>
2024-04-29 08:40:05 -0300

    menu criado

--- mexendo-com-arquivos/exercicio-pratico.py
+++ mexendo-com-arquivos/exercicio-pratico.py
@@ -1,7 +1,28 @@
-contato = {}
+#Cadastrar
+contatos = []
+contato1 = {}
 
-contato["nome"] = input("Digite aqui o seu nome\n=>")
-contato["email"] = input("Agora digite seu e-mail\n=>")
-contato["tele"] = input("E qual séria o seu telefone?\n=>")
-contato["peso"] = float(input("QUal é o seu peso?\n=>"))
-contato["idade"] = int(input("E por ultimo qual a sua idade?\n=>"))
+contato1["nome"] = input("Digite aqui o seu nome\n=>")
+contato1["email"] = input("Agora digite seu e-mail\n=>")
+contato1["tele"] = input("E qual séria o seu telefone?\n=>")
+contato1["peso"] = float(input("QUal é o seu peso?\n=>"))
+contato1["idade"] = int(input("E por ultimo qual a sua idade?\n=>"))
+contatos.append(contato1)
+
+
+#Menu
+menu = ("""---------------------------------------- +
+        \nBEM VINDO + 
+        \n\n Essas são as funções que temos dísponivel no momento
+        \n[1] - Cadastrar 
+        \n[2] - Listar 
+        \n[3] - Procurar
+        \n[4] - Backup em um arquivo
+        \n[5] - Restaurar de um arquivo
+        \n[6] - Sair 
+        \n----------------------------------------""")
+print(menu)
+decisao = int(input("\Digite o valor equivalente a função desejada por favor"))
+
+print("Programa encerrado")
+
