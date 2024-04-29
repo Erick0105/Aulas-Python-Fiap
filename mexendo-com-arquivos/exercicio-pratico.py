7e74e17b2733c635db1a43486c11f3662518bb2e
Erick0105 <erickspot01@gmail.com>
2024-04-29 08:15:45 -0300

    recibimento de informações concluido

--- mexendo-com-arquivos/exercicio-pratico.py
+++ mexendo-com-arquivos/exercicio-pratico.py
@@ -1,1 +1,7 @@
 contato = {}
+
+contato["nome"] = input("Digite aqui o seu nome\n=>")
+contato["email"] = input("Agora digite seu e-mail\n=>")
+contato["tele"] = input("E qual séria o seu telefone?\n=>")
+contato["peso"] = float(input("QUal é o seu peso?\n=>"))
+contato["idade"] = int(input("E por ultimo qual a sua idade?\n=>"))
