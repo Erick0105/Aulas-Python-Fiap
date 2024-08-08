ffc65c08f7d5ea9e958ecc6c3e070b82bffa8098
Erick0105 <erickspot01@gmail.com>
2024-08-08 11:37:42 -0300

    exercicio simplificado

--- filtrando/ex1.py
+++ filtrando/ex1.py
@@ -2,10 +2,7 @@
 listaNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
 def dividindo (numero):
-    if numero % 3 == 0 or numero % 4 == 0:
-        return True
-    else:
-        return False
+    return numero % 3 == 0 or numero % 4 == 0
     
 novaLista = list(filter(dividindo,listaNum))
 
